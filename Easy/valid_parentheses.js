a500896ee927f56883713cd76bc3bd86be26c7c2
AldythNahak <aldyth3599nahak@gmail.com>
2025-05-16 00:28:58 +0700

    solve valid_parentheses.js

--- Easy/valid_parentheses.js
+++ Easy/valid_parentheses.js
@@ -15,87 +15,33 @@ var isValid = function (s) {
     "{": "}",
   };
   const mapParenthesesList = Object.entries(mapParentheses).flat();
-  const oddNumber = Array.from({ length: lenS }, (_, i) => i + 1).filter(n => n % 2 === 1);
-  var indexMatch = [];
+
+  var collectParentheses = [];
 
   for (let i = 0; i < lenS; i++) {
-    let currStr = s[i];
-    let nextStrMatch = "";
-    let lastStrMatch = "";
+    if(i == 0 && Object.values(mapParentheses).includes(s[i])) {
+      return false;
+    }
 
-    if (indexMatch.includes(i)) {
+    if(!mapParenthesesList.includes(s[i])) {
+      return false;
+    }
+
+    if(typeof mapParentheses[s[i]] !== "undefined") {
+      collectParentheses.push(s[i]);
       continue;
     }
-    
-    for(let odd in oddNumber) {
-        if(odd + i < lenS) {
-            nextStrMatch = s[i + odd]
-            const isMatch = [currStr, nextStrMatch]
-                .filter((v) => v !== "")
-                .every((val) => mapParenthesesList.includes(val));
 
-            if(!isMatch) {
-                return false;
-            }
-
-            if(mapParentheses[currStr] == nextStrMatch) {
-                indexMatch.push(i);
-                indexMatch.push(i + odd);
-                break;
-            }
-        }
+    lastCheck = collectParentheses.slice(-1);
+    if(lastCheck !== "undefined" && mapParentheses[lastCheck] == s[i]) {
+      collectParentheses.pop();
+      continue;
+    } else {
+      return false;
     }
-
-    // if(i + 1 < lenS) {
-    //     nextStrMatch = s[i + 1];
-    // }
-
-    // if (i + 1 < lenS - 1 - i) {
-    //   lastStrMatch = s[lenS - 1 - i];
-    // }
-
-    // const isMatch = [currStr, nextStrMatch, lastStrMatch]
-    //   .filter((v) => v !== "")
-    //   .every((val) => mapParenthesesList.includes(val));
-
-    // if (!isMatch) {
-    //   return false;
-    // }
-
-    // if (indexMatch.includes(i)) {
-    //   continue;
-    // }
-
-    // // console.log(
-    // //   `${i}, C = ${i}-${currStr} ; N = ${i + 1}-${nextStrMatch} ; L = ${
-    // //     lenS - 1 - i
-    // //   }-${lastStrMatch}`
-    // // );
-
-    // if (typeof mapParentheses[currStr] !== "undefined") {
-    //   if (nextStrMatch !== "" && mapParentheses[currStr] == nextStrMatch) {
-    //     indexMatch.push(i);
-    //     indexMatch.push(i + 1);
-    //     continue;
-    //   } else if (
-    //     lastStrMatch !== "" &&
-    //     mapParentheses[currStr] == lastStrMatch
-    //   ) {
-    //     indexMatch.push(i);
-    //     indexMatch.push(lenS - 1 - i);
-    //     continue;
-    //   } else {
-    //     return false;
-    //   }
-    // }
-
-    // return false;
   }
 
-
-  return lenS == indexMatch.length;
-
-  return true;
+  return collectParentheses.length == 0;
 };
 
 console.log(isValid("(]")); //false
@@ -104,4 +50,4 @@ console.log(isValid("([])")); //true
 console.log(isValid("(}{)")); //false
 console.log(isValid("([{}])")); //true
 console.log(isValid("(){}}{")); //false
-console.log(isValid("(([]){})")); //false
+console.log(isValid("(([]){})")); //true
