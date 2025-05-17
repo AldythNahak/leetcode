30a77daf5bd07b10fdfea5a68780666ba7147589
AldythNahak <aldyth3599nahak@gmail.com>
2025-05-18 00:49:20 +0700

    add case

---
+++ Medium/longest_palindrome_substring.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  if (s.length == 0 || s.length > 1000) {
+    return s;
+  }
+
+  if (s == s.split("").reverse().join("")) {
+    return s;
+  }
+
+  var resultMatch = s[0];
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j <= s.length; j++) {
+      if (s[i] != s[j - 1]) {
+        continue;
+      }
+
+      if (j > 1 && i + 1 < s.length && s[i + 1] != s[j - 2]) {
+        continue;
+      }
+
+      if (resultMatch.length >= j - i) {
+        continue;
+      }
+
+      let strJoin = s.slice(i, j);
+      let strJoinReverse = strJoin.split("").reverse().join("");
+
+      if (strJoin == strJoinReverse) {
+        resultMatch = strJoin;
+      }
+    }
+  }
+
+  return resultMatch;
+};
+
+console.log(longestPalindrome("cbbd"));
+console.log(longestPalindrome("babad"));
+console.log(longestPalindrome("ccd"));
+console.log(longestPalindrome("xaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuuvvvvvvvvvvwwwwwwwwwwxxxxxxxxxxyyyyyyyyyyzzzzzzzzzzyyyyyyyyyyxxxxxxxxxxwwwwwwwwwwvvvvvvvvvvuuuuuuuuuuttttttttttssssssssssrrrrrrrrrrqqqqqqqqqqppppppppppoooooooooonnnnnnnnnnmmmmmmmmmmllllllllllkkkkkkkkkkjjjjjjjjjjiiiiiiiiiihhhhhhhhhhggggggggggffffffffffeeeeeeeeeeddddddddddccccccccccbbbbbbbbbbaaaaaaaabbbbbbbbbbccccccccccddddddddddeeeeeeeeeeffffffffffgggggggggghhhhhhhhhhiiiiiiiiiijjjjjjjjjjkkkkkkkkkkllllllllllmmmmmmmmmmnnnnnnnnnnooooooooooppppppppppqqqqqqqqqqrrrrrrrrrrssssssssssttttttttttuuuuuuuuuu"));
