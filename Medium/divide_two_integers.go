6eb6a1f13e68fe244b39847a44d2e11fe9e3c769
AldythNahak <aldyth3599nahak@gmail.com>
2025-05-19 23:34:20 +0700

    add case

--- Medium/divide_two_integers.go
+++ Medium/divide_two_integers.go
@@ -6,28 +6,37 @@ import (
 )
 
 func divide(dividend int, divisor int) int {
-	if dividend < -(1 << 31) {
-		dividend = -(1 << 31)
-	} else if dividend > (1<<31)-1 {
-		dividend = (1 << 31) - 1
+	const INT_MIN = -(1 << 31)
+	const INT_MAX = (1 << 31) - 1
+
+	if dividend < INT_MIN {
+		dividend = INT_MIN
 	}
 
-	if divisor < -(1 << 31) {
-		divisor = -(1 << 31)
-	} else if divisor > (1<<31)-1 {
-		divisor = (1 << 31) - 1
+	if divisor > INT_MAX {
+		divisor = INT_MAX
 	}
 
 	if divisor == 0 {
 		return 0
 	}
 
-	return int(math.RoundToEven(float64(dividend) / float64(divisor)))
+	divideNumber := dividend / divisor
+	divideNumber = int(math.RoundToEven(float64(divideNumber)))
+
+	if divideNumber > INT_MAX {
+		return INT_MAX
+	} else if divideNumber < INT_MIN {
+		return INT_MIN
+	}
+
+	return divideNumber
 }
 
 func run_divide() {
-	fmt.Println(divide(10, 3))
-	fmt.Println(divide(7, -3))
-	fmt.Println(divide(-2147483648, -1))
+	fmt.Println(divide(10, 3))           //3
+	fmt.Println(divide(7, -3))           //-2
+	fmt.Println(divide(-2147483648, -1)) //2147483647
+	fmt.Println(divide(2147483647, 2))   //1073741823
 
 }
