02254f086ec1646114935306f9e2a806f0197790
Aldyth Nahak <49281877+AldythNahak@users.noreply.github.com>
2025-05-20 15:36:45 +0700

    add case

--- Hard/substring_with_concatenation_of_all_words.py
+++ Hard/substring_with_concatenation_of_all_words.py
@@ -1,6 +1,4 @@
-import itertools
-import random
-
+from collections import Counter
 
 class Solution(object):
     def findSubstring(self, s, words):
@@ -9,17 +7,53 @@ class Solution(object):
         :type words: List[str]
         :rtype: List[int]
         """    
-        
-        listCheckWords = list(set(words))
-        if len([s.find(k) for k in listCheckWords if s.find(k) >= 0]) == 0:
+        # Simple but Limited time out
+        # permutations = itertools.permutations(words)
+        # collectKeywords = list(set(["".join(p) for p in permutations]))
+        # indexMatch = sorted([i for k in collectKeywords for i in range(len(s)) if s.startswith(k, i)])
+        # return indexMatch
+
+        if not words or len(words) > 5000:
             return []
         
-        permutations = itertools.permutations(words)
-        collectKeywords = list(set(["".join(p) for p in permutations]))
-        indexMatch = sorted([i for k in collectKeywords for i in range(len(s)) if s.startswith(k, i)])
+        if not s or len(s) > 10**4:
+            return []
+    
+        lenText = len(s)
+        lenKeyword = sum(len(k) for k in words)
+        
+        if lenText < lenKeyword:
+            return []
+        
+        totalKeyword = len(words)
+        counterKeyword = Counter(words)        
+        indexMatch = []
+        
+        for i in range(len(s) - lenKeyword + 1):
+            counterKeywordCheck = counterKeyword.copy()
+            startIndex = i
+            totalMatch = 0
+
+            while totalMatch < totalKeyword:
+                isMatch = False
+                for keyword in counterKeywordCheck:
+                    if counterKeywordCheck[keyword] > 0 and str(s).startswith(keyword, startIndex):
+                        counterKeywordCheck[keyword] -= 1
+                        startIndex += len(keyword)
+                        totalMatch += 1
+                        isMatch = True
+                        break
+
+                if not isMatch:
+                    break
+
+            if totalMatch == totalKeyword:
+                indexMatch.append(i)
 
         return indexMatch
+                
         
 print(Solution().findSubstring("barfoothefoobarman", ["foo","bar"])) #[0,9]
-print(Solution().findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"])) #[0,9]
+# print(Solution().findSubstring("barfoofoobarthefoobarman", ["bar","foo","the"])) #[0,9]
+# print(Solution().findSubstring("foobarfoobar", ["foo","bar"])) #[0,3,6]
 print(Solution().findSubstring("foobarfoobar", ["foo","bar"])) #[0,3,6]
